fa41156f47843773408489a96e8084f406a4f297
clavat0 <114899547+clavat0@users.noreply.github.com>
2023-08-06 22:50:28 -0600

    a

--- pacientes-api/src/main/java/edu/uady/pacientesapi/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.uady.pacientesapi.repository;
-
-import edu.uady.pacientesapi.entity.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository  extends JpaRepository<Paciente, Long> {
-}
